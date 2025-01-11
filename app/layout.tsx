5b4dd92aa3f6b94b3de358614c934cba32064671
Talal Al Ajmi <talalajmi98@gmail.com>
2025-01-11 23:27:19 +0400

    Metadata and icons added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,26 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const inter = localFont({
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 700 800 900",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const spaceGrotesk = localFont({
+  src: "./fonts/SpaceGroteskVF.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 700 ",
 });
 
 export const metadata: Metadata = {
   title: "Devflow",
-  description: "A different version of StackOverflow",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
+  description:
+    "A different version of StackOverflow is a Q&A platform designed to provide a unique and improved user experience for developers seeking solutions to their coding problems. This platform offers enhanced features, a modern interface, and a more engaging community-driven approach to knowledge sharing.",
 };
 
 export default function RootLayout({
@@ -25,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div className="font-black text-violet-300 text-3xl  ">hello</div>;
+  return (
+    <>
+      <div className="h1-bold font-space-grotesk flex items-center">
+        Dev
+        <p className="text-primary-500">Flow</p>
+      </div>
+      <div className="h1-bold font-inter">
+        Hello, this is my new application witch. Yuh
+      </div>
+    </>
+  );
 }
